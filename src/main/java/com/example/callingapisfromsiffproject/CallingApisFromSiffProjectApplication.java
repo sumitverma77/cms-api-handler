553c4953e9782a81fc88822444e133b545a8e34b
Sumit Verma <987sumitverma@gmail.com>
2024-06-02 12:51:33 +0530

    Initial commit

---
+++ src/main/java/com/example/callingapisfromsiffproject/CallingApisFromSiffProjectApplication.java
@@ -0,0 +1,14 @@
+package com.example.callingapisfromsiffproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallingApisFromSiffProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CallingApisFromSiffProjectApplication.class, args);
+        System.out.println("project is working");
+    }
+
+}
---
+++ src/test/java/com/example/callingapisfromsiffproject/CallingApisFromSiffProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.callingapisfromsiffproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallingApisFromSiffProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
