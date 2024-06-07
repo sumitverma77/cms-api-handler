5d1826d198536351ca00182dd65a3359a9b3f424
Sumit Verma <987sumitverma@gmail.com>
2024-06-07 16:06:24 +0530

    Calling Services of ContactManagement System using Rest Template Method

--- src/main/java/com/example/callingapisfromsiffproject/controller/AddController.java
+++ src/main/java/com/example/callingapisfromsiffproject/controller/AddController.java
@@ -1,7 +1,9 @@
 package com.example.callingapisfromsiffproject.controller;
 
 import com.example.callingapisfromsiffproject.request.AddRequest;
+import com.example.callingapisfromsiffproject.request.SearchByBothRequest;
 import com.example.callingapisfromsiffproject.response.AddResponse;
+import com.example.callingapisfromsiffproject.response.SearchResponse;
 import com.example.callingapisfromsiffproject.service.ContactService;
 import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +14,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("call/")
 public class AddController {
@@ -21,5 +25,10 @@ public class AddController {
     @PostMapping("add/api")
     public AddResponse add(@RequestBody AddRequest addRequest) {
         return contactService.addService(addRequest);
+    }
+    @GetMapping("search/api")
+    public List<SearchResponse> search(@RequestBody SearchByBothRequest searchByBothRequest)
+    {
+        return contactService.searchByBoth(searchByBothRequest);
     }
 }
